b5bb1de53a896d26a7d4a603a5f8304272fc365e
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 18:55:31 -0300

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -53,49 +53,139 @@ const Navbar = () => {
           </button>
         </div>
         <ul className={`md:flex space-x-4 items-center md:flex-row ${menuOpen ? 'block' : 'hidden'} md:block`}>
-          {['Início', 'Gêneros', 'Assistidos', 'Ver Depois', 'Populares', 'Alta Avaliação', 'Agora Em Cartaz', 'Em Breve'].map((text, index) => (
-            <li key={index}>
-              <Link
-                to={`/${text.toLowerCase().replace(/\s+/g, '-')}`}
-                className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink(`/${text.toLowerCase().replace(/\s+/g, '-')}`) ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
-              >
-                {text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div className={`md:flex space-x-4 items-center ${searchOpen ? 'block' : 'hidden'} md:block`}>
-          <div className="relative">
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={handleSearchChange}
-              onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
-              className="w-full px-4 py-2 rounded-full bg-neutral-800 text-white placeholder-white focus:outline-none focus:ring-2 focus:ring-[#bd0003]"
-              placeholder="Pesquisar filmes"
-            />
-            <button
-              onClick={handleSearch}
-              className="absolute right-2 top-2 text-[#bd0003]"
+          <li>
+            <Link
+              to="/"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
             >
-              <FaSearch size={20} />
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className={`absolute top-16 right-0 mt-2 bg-black text-white w-56 ${menuOpen ? 'block' : 'hidden'} md:hidden`}>
-        <ul>
-          {['Início', 'Gêneros', 'Assistidos', 'Ver Depois', 'Populares', 'Alta Avaliação', 'Agora Em Cartaz', 'Em Breve'].map((text, index) => (
-            <li key={index}>
+              Início
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/genres"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/genres') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Gêneros
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/watched-movies"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/watched-movies') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Assistidos
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/to-watch-movies"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/to-watch-movies') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Ver Depois
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/popular-movies"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/popular-movies') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Populares
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/top-rated-movies"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/top-rated-movies') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Alta Avaliação
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/now-playing-movies"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/now-playing-movies') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Agora Em Cartaz
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/upcoming-movies"
+              className={`text-[#bd0003] hover:text-gray-300 ${isActiveLink('/upcoming-movies') ? 'border-2 border-[#bd0003] text-white' : ''} px-2 py-1 rounded-full`}
+            >
+              Em Breve
+            </Link>
+          </li>
+        </ul>
+        <div className={`absolute top-16 right-0 mt-2 bg-black text-white w-56 ${menuOpen ? 'block' : 'hidden'} md:hidden`}>
+          <ul>
+            <li>
               <Link
-                to={`/${text.toLowerCase().replace(/\s+/g, '-')}`}
-                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink(`/${text.toLowerCase().replace(/\s+/g, '-')}`) ? 'border-2 border-[#bd0003] text-white' : ''}`}
+                to="/"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/') ? 'border-2 border-[#bd0003] text-white' : ''}`}
               >
-                {text}
+                Início
               </Link>
             </li>
-          ))}
-        </ul>
+            <li>
+              <Link
+                to="/genres"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/genres') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Gêneros
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/watched-movies"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/watched-movies') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Assistidos
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/to-watch-movies"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/to-watch-movies') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Ver Depois
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/popular-movies"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/popular-movies') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Populares
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/top-rated-movies"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/top-rated-movies') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Alta Avaliação
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/now-playing-movies"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/now-playing-movies') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Agora Em Cartaz
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/upcoming-movies"
+                className={`block py-2 px-4 text-[#bd0003] hover:text-gray-300 ${isActiveLink('/upcoming-movies') ? 'border-2 border-[#bd0003] text-white' : ''}`}
+              >
+                Em Breve
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
