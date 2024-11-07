481a3d5142195d256573073722f6db4c360634d0
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 17:49:50 -0300

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -22,17 +22,93 @@ const Navbar = () => {
         >
           Tela<span className="text-white">Viva</span>
         </Link>
-        <div className="md:hidden">
+        <div className="md:hidden relative">
           <button
             onClick={toggleMenu}
             className="text-white transition-transform duration-300 ease-in-out transform hover:scale-105"
           >
             {menuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
           </button>
+          {menuOpen && (
+            <div className="absolute top-12 right-0 bg-black text-white w-56 py-4 px-6 shadow-lg z-50">
+              <ul className="space-y-4">
+                <li>
+                  <Link
+                    to="/"
+                    onClick={() => handleLinkClick('/')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Início
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/genres"
+                    onClick={() => handleLinkClick('/genres')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/genres' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Gêneros
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/watched-movies"
+                    onClick={() => handleLinkClick('/watched-movies')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/watched-movies' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Assistidos
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/to-watch-movies"
+                    onClick={() => handleLinkClick('/to-watch-movies')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/to-watch-movies' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Ver Depois
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/popular-movies"
+                    onClick={() => handleLinkClick('/popular-movies')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/popular-movies' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Populares
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/top-rated-movies"
+                    onClick={() => handleLinkClick('/top-rated-movies')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/top-rated-movies' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Alta Avaliação
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/now-playing-movies"
+                    onClick={() => handleLinkClick('/now-playing-movies')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/now-playing-movies' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Agora Em Cartaz
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/upcoming-movies"
+                    onClick={() => handleLinkClick('/upcoming-movies')}
+                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/upcoming-movies' ? 'bg-[#bd0003] text-white' : ''}`}
+                  >
+                    Em Breve
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
-        <ul
-          className="md:flex space-x-4 items-center md:flex-row transition-all duration-300 ease-in-out hidden md:block"
-        >
+        <ul className="md:flex space-x-4 items-center md:flex-row transition-all duration-300 ease-in-out hidden md:block">
           <li>
             <Link
               to="/"
@@ -107,84 +183,6 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      {menuOpen && (
-        <div className="md:hidden absolute left-0 right-0 top-full bg-black text-white py-4 px-6 mt-4">
-          <ul className="space-y-4">
-            <li>
-              <Link
-                to="/"
-                onClick={() => handleLinkClick('/')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Início
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/genres"
-                onClick={() => handleLinkClick('/genres')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/genres' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Gêneros
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/watched-movies"
-                onClick={() => handleLinkClick('/watched-movies')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/watched-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Assistidos
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/to-watch-movies"
-                onClick={() => handleLinkClick('/to-watch-movies')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/to-watch-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Ver Depois
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/popular-movies"
-                onClick={() => handleLinkClick('/popular-movies')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/popular-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Populares
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/top-rated-movies"
-                onClick={() => handleLinkClick('/top-rated-movies')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/top-rated-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Alta Avaliação
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/now-playing-movies"
-                onClick={() => handleLinkClick('/now-playing-movies')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/now-playing-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Agora Em Cartaz
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/upcoming-movies"
-                onClick={() => handleLinkClick('/upcoming-movies')}
-                className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/upcoming-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-              >
-                Em Breve
-              </Link>
-            </li>
-          </ul>
-        </div>
-      )}
     </nav>
   );
 };
