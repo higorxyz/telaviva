29b413baaf69fdc51dfa94dc61dd694786ccd9d5
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 17:55:38 -0300

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,185 +1,74 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { Link, useNavigate } from 'react-router-dom';
+import { FaBars, FaTimes, FaSearch } from 'react-icons/fa';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [activeLink, setActiveLink] = useState('/');
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
-  const handleLinkClick = (link) => {
-    setActiveLink(link);
+  const handleSearchChange = (e) => setSearchQuery(e.target.value);
+
+  const handleSearch = () => {
+    if (searchQuery.trim() === '') return;
+    navigate(`/search-results?query=${searchQuery}`);
   };
 
   return (
-    <nav className="bg-black text-white py-4 px-6 shadow-lg transition-shadow">
+    <nav className="bg-black text-white py-4 px-6">
       <div className="flex items-center justify-between">
-        <Link
-          to="/"
-          className="text-3xl font-bold text-[#bd0003]"
-          style={{ fontFamily: 'Verdana, sans-serif', fontWeight: 'bold' }}
-        >
+        <Link to="/" className="text-3xl font-bold text-red-500" style={{ fontFamily: 'Verdana, sans-serif', fontWeight: 'bold' }}>
           Tela<span className="text-white">Viva</span>
         </Link>
-        <div className="md:hidden relative">
-          <button
-            onClick={toggleMenu}
-            className="text-white transition-transform duration-300 ease-in-out transform hover:scale-105"
-          >
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="text-white">
             {menuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
           </button>
-          {menuOpen && (
-            <div className="absolute top-12 right-0 bg-black text-white w-56 py-4 px-6 shadow-lg z-50">
-              <ul className="space-y-4">
-                <li>
-                  <Link
-                    to="/"
-                    onClick={() => handleLinkClick('/')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Início
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/genres"
-                    onClick={() => handleLinkClick('/genres')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/genres' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Gêneros
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/watched-movies"
-                    onClick={() => handleLinkClick('/watched-movies')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/watched-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Assistidos
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/to-watch-movies"
-                    onClick={() => handleLinkClick('/to-watch-movies')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/to-watch-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Ver Depois
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/popular-movies"
-                    onClick={() => handleLinkClick('/popular-movies')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/popular-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Populares
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/top-rated-movies"
-                    onClick={() => handleLinkClick('/top-rated-movies')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/top-rated-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Alta Avaliação
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/now-playing-movies"
-                    onClick={() => handleLinkClick('/now-playing-movies')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/now-playing-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Agora Em Cartaz
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/upcoming-movies"
-                    onClick={() => handleLinkClick('/upcoming-movies')}
-                    className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/upcoming-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-                  >
-                    Em Breve
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          )}
         </div>
-        <ul className="md:flex space-x-4 items-center md:flex-row transition-all duration-300 ease-in-out hidden md:block">
+        <ul className={`md:flex space-x-4 items-center md:flex-row ${menuOpen ? 'block' : 'hidden'} md:block`}>
           <li>
-            <Link
-              to="/"
-              onClick={() => handleLinkClick('/')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Início
-            </Link>
+            <Link to="/" className="text-red-500 hover:text-gray-300">Início</Link>
           </li>
           <li>
-            <Link
-              to="/genres"
-              onClick={() => handleLinkClick('/genres')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/genres' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Gêneros
-            </Link>
+            <Link to="/genres" className="text-red-500 hover:text-gray-300">Gêneros</Link>
           </li>
           <li>
-            <Link
-              to="/watched-movies"
-              onClick={() => handleLinkClick('/watched-movies')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/watched-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Assistidos
-            </Link>
+            <Link to="/watched-movies" className="text-red-500 hover:text-gray-300">Assistidos</Link>
           </li>
           <li>
-            <Link
-              to="/to-watch-movies"
-              onClick={() => handleLinkClick('/to-watch-movies')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/to-watch-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Ver Depois
-            </Link>
+            <Link to="/to-watch-movies" className="text-red-500 hover:text-gray-300">Ver Depois</Link>
           </li>
           <li>
-            <Link
-              to="/popular-movies"
-              onClick={() => handleLinkClick('/popular-movies')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/popular-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Populares
-            </Link>
+            <Link to="/popular-movies" className="text-red-500 hover:text-gray-300">Populares</Link>
           </li>
           <li>
-            <Link
-              to="/top-rated-movies"
-              onClick={() => handleLinkClick('/top-rated-movies')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/top-rated-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Alta Avaliação
-            </Link>
+            <Link to="/top-rated-movies" className="text-red-500 hover:text-gray-300">Alta Avaliação</Link>
           </li>
           <li>
-            <Link
-              to="/now-playing-movies"
-              onClick={() => handleLinkClick('/now-playing-movies')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/now-playing-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Agora Em Cartaz
-            </Link>
+            <Link to="/now-playing-movies" className="text-red-500 hover:text-gray-300">Agora Em Cartaz</Link>
           </li>
           <li>
-            <Link
-              to="/upcoming-movies"
-              onClick={() => handleLinkClick('/upcoming-movies')}
-              className={`text-[#bd0003] hover:text-gray-300 hover:shadow-lg px-2 py-1 rounded-md transition-all duration-300 ${activeLink === '/upcoming-movies' ? 'bg-[#bd0003] text-white' : ''}`}
-            >
-              Em Breve
-            </Link>
+            <Link to="/upcoming-movies" className="text-red-500 hover:text-gray-300">Em Breve</Link>
+          </li>
+          <li>
+            <div className="relative">
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchChange}
+                onKeyPress={(e) => e.key === 'Enter' && handleSearch()}
+                className="px-4 py-2 rounded-full bg-neutral-800 text-white placeholder-white focus:outline-none focus:ring-2 focus:ring-red-500"
+                placeholder="Pesquisar filmes"
+              />
+              <button
+                onClick={handleSearch}
+                className="absolute right-2 top-2 text-red-500"
+              >
+                <FaSearch size={20} />
+              </button>
+            </div>
           </li>
         </ul>
       </div>
