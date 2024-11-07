f992796c9b79f8b19a0c27c635a3fb4ce9e072dc
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 16:58:51 -0300

    Update TopRatedMovies.jsx

--- src/pages/TopRatedMovies.jsx
+++ src/pages/TopRatedMovies.jsx
@@ -39,7 +39,7 @@ const TopRatedMovies = () => {
   return (
     <div className="p-6 bg-neutral-950 text-white">
       <h1 className="text-4xl font-bold mb-4">Filmes Mais Bem Avaliados</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
