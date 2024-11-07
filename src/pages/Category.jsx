1f81f2886235025a59687ba18c0811811b13287b
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 16:48:27 -0300

    Update Category.jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -50,7 +50,7 @@ const Category = () => {
   return (
     <div className="p-6 bg-neutral-950 text-white">
       <h1 className="text-4xl font-bold mb-4">{categoryName}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
