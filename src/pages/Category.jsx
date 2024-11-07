a1cb818725592c893e3d5bd89d44abee6c4afc5d
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 16:26:27 -0300

    Update Category.jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchMoviesByCategory, fetchCategories } from '../api';
+import MovieCard from '../components/MovieCard';
+import { waveform } from 'ldrs';
+
+waveform.register();
 
 const Category = () => {
   const { category } = useParams();
   const [movies, setMovies] = useState([]);
   const [categoryName, setCategoryName] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const getMoviesByCategory = async () => {
-      const moviesData = await fetchMoviesByCategory(category);
-      setMovies(moviesData);
+      try {
+        const moviesData = await fetchMoviesByCategory(category);
+        setMovies(moviesData);
+      } catch (err) {
+        setError('Erro ao carregar os filmes.');
+      } finally {
+        setLoading(false);
+      }
     };
 
     const getCategoryName = async () => {
@@ -23,15 +35,24 @@ const Category = () => {
     getCategoryName();
   }, [category]);
 
+  if (loading) return (
+    <div className="flex items-center justify-center min-h-screen bg-neutral-950">
+      <l-waveform size="35" stroke="3.5" speed="1" color="red"></l-waveform>
+    </div>
+  );
+
+  if (error) return (
+    <div className="flex items-center justify-center min-h-screen bg-neutral-950 text-red-500">
+      <p>{error}</p>
+    </div>
+  );
+
   return (
-    <div className="p-6 bg-neutral-950 min-h-screen text-white">
-      <h1 className="text-3xl font-bold mb-4">{categoryName}</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="p-6 bg-neutral-950 text-white">
+      <h1 className="text-4xl font-bold mb-4">{categoryName}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4">
         {movies.map((movie) => (
-          <div key={movie.id} className="bg-neutral-800 rounded-lg p-4">
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="rounded-md mb-2" />
-            <h2 className="text-xl font-semibold">{movie.title}</h2>
-          </div>
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
