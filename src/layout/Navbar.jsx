c3af3fe81c106801feb92165ad9ab6d97a98d9f8
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 18:38:49 -0300

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,9 +5,14 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState('');
 
   const toggleMenu = () => setMenuOpen(!menuOpen);
   const toggleSearch = () => setSearchOpen(!searchOpen);
+
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
 
   return (
     <nav className="bg-black text-white py-4 px-6 transition-all ease-in-out duration-300">
@@ -19,7 +24,7 @@ const Navbar = () => {
         >
           Tela<span className="text-white">Viva</span>
         </Link>
-        <div className="md:hidden flex items-center">
+        <div className="md:hidden flex items-center ml-4">
           <button onClick={toggleSearch} className="text-white mr-4 transition-transform duration-300 hover:scale-110">
             {searchOpen ? <FaTimes size={25} /> : <FaBars size={30} />}
           </button>
@@ -27,29 +32,37 @@ const Navbar = () => {
             <input
               type="text"
               placeholder="Buscar filme..."
-              className="text-black p-2 rounded-md w-40 transition-all duration-300"
+              className="text-black p-2 rounded-lg w-40 transition-all duration-300"
             />
           )}
         </div>
-        <div className="md:flex hidden items-center space-x-6">
-          <ul className="flex space-x-6 items-center transition-all ease-in-out duration-300">
+        <div className="md:flex hidden items-center space-x-8 ml-6">
+          <ul className="flex space-x-8 items-center transition-all ease-in-out duration-300">
             {['Em Cartaz', 'Populares', 'Alta Avaliação', 'Em Breve', 'Ver Depois', 'Assistidos', 'Gêneros'].map((text, index) => (
-              <li key={index} className="relative group">
+              <li
+                key={index}
+                className={`relative group rounded-lg transition-all duration-300 ${
+                  activeLink === text ? 'bg-[#bd0003] text-white' : 'text-[#bd0003] hover:text-white'
+                }`}
+              >
                 <Link
                   to={`/${text.toLowerCase().replace(/\s+/g, '-')}`}
-                  className="text-[#bd0003] hover:text-white transition-all duration-300 group-hover:text-white px-4 py-2 rounded-lg"
+                  onClick={() => handleLinkClick(text)}
+                  className="px-4 py-2 transition-all duration-300"
                 >
                   {text}
                 </Link>
-                <span className="absolute inset-0 bg-[#bd0003] opacity-0 group-hover:opacity-100 transition-opacity duration-300"></span>
+                {activeLink === text && (
+                  <span className="absolute inset-0 bg-[#bd0003] opacity-80 transition-opacity duration-300"></span>
+                )}
               </li>
             ))}
           </ul>
-          <div className="relative">
+          <div className="relative ml-8">
             <input
               type="text"
               placeholder="Buscar filme..."
-              className="text-black p-2 rounded-md w-64 transition-all duration-300"
+              className="text-black p-2 rounded-lg w-64 transition-all duration-300"
             />
           </div>
         </div>
@@ -60,7 +73,10 @@ const Navbar = () => {
             <li key={index} className="w-full">
               <Link
                 to={`/${text.toLowerCase().replace(/\s+/g, '-')}`}
-                className="text-[#bd0003] hover:text-white transition-all duration-300 w-full text-center py-2 rounded-lg"
+                onClick={() => handleLinkClick(text)}
+                className={`text-[#bd0003] hover:text-white transition-all duration-300 w-full text-center py-2 rounded-lg ${
+                  activeLink === text ? 'bg-[#bd0003] text-white' : ''
+                }`}
               >
                 {text}
               </Link>
