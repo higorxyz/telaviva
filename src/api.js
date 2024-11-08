f4d202eb276f1205d4bc46f64ebc55485ca18020
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 22:30:16 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,72 +1,28 @@
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-export const fetchPopularMovies = async () => {
-  let page = 1;
-  let allResults = [];
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await fetch(`${API_BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${page}`);
-    const data = await response.json();
-    
-    allResults = [...allResults, ...data.results];
-    hasMore = page < data.total_pages;
-    page++;
-  }
-
-  return allResults;
+export const fetchPopularMovies = async (page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+  const data = await response.json();
+  return data;
 };
 
-export const fetchTopRatedMovies = async () => {
-  let page = 1;
-  let allResults = [];
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await fetch(`${API_BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-BR&page=${page}`);
-    const data = await response.json();
-    
-    allResults = [...allResults, ...data.results];
-    hasMore = page < data.total_pages;
-    page++;
-  }
-
-  return allResults;
+export const fetchTopRatedMovies = async (page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+  const data = await response.json();
+  return data;
 };
 
-export const fetchNowPlayingMovies = async () => {
-  let page = 1;
-  let allResults = [];
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await fetch(`${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=pt-BR&page=${page}`);
-    const data = await response.json();
-    
-    allResults = [...allResults, ...data.results];
-    hasMore = page < data.total_pages;
-    page++;
-  }
-
-  return allResults;
+export const fetchNowPlayingMovies = async (page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+  const data = await response.json();
+  return data;
 };
 
-export const fetchUpcomingMovies = async () => {
-  let page = 1;
-  let allResults = [];
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await fetch(`${API_BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR&page=${page}`);
-    const data = await response.json();
-    
-    allResults = [...allResults, ...data.results];
-    hasMore = page < data.total_pages;
-    page++;
-  }
-
-  return allResults;
+export const fetchUpcomingMovies = async (page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+  const data = await response.json();
+  return data;
 };
 
 export const fetchMovieDetails = async (movieId) => {
@@ -75,7 +31,7 @@ export const fetchMovieDetails = async (movieId) => {
     throw new Error('Erro ao buscar detalhes do filme');
   }
   const data = await response.json();
-  
+
   const translation = data.translations?.translations.find(
     (t) => t.iso_639_1 === 'pt' && t.data.title
   );
@@ -86,21 +42,10 @@ export const fetchMovieDetails = async (movieId) => {
   return data;
 };
 
-export const fetchMoviesByCategory = async (categoryId) => {
-  let page = 1;
-  let allResults = [];
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await fetch(`${API_BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}&language=pt-BR&page=${page}`);
-    const data = await response.json();
-    
-    allResults = [...allResults, ...data.results];
-    hasMore = page < data.total_pages;
-    page++;
-  }
-
-  return allResults;
+export const fetchMoviesByCategory = async (categoryId, page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}&language=pt-BR&page=${page}`);
+  const data = await response.json();
+  return data;
 };
 
 export const fetchMovieTrailer = async (movieId) => {
@@ -109,7 +54,7 @@ export const fetchMovieTrailer = async (movieId) => {
     throw new Error('Erro ao buscar trailer do filme');
   }
   const data = await response.json();
-  return data.results.length > 0 ? data.results[0] : null; 
+  return data.results.length > 0 ? data.results[0] : null;
 };
 
 export const fetchCategories = async () => {
@@ -127,19 +72,8 @@ export const fetchMovieCast = async (movieId) => {
   return data.cast;
 };
 
-export const fetchMoviesBySearch = async (query) => {
-  let page = 1;
-  let allResults = [];
-  let hasMore = true;
-
-  while (hasMore) {
-    const response = await fetch(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=pt-BR&page=${page}`);
-    const data = await response.json();
-    
-    allResults = [...allResults, ...data.results];
-    hasMore = page < data.total_pages;
-    page++;
-  }
-
-  return allResults;
+export const fetchMoviesBySearch = async (query, page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=pt-BR&page=${page}`);
+  const data = await response.json();
+  return data;
 };
