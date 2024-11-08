f7c194ad73e3bfa813f23873f6f58b0786959dca
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 22:22:59 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,27 +2,71 @@ const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 export const fetchPopularMovies = async () => {
-  const response = await fetch(`${API_BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR`);
-  const data = await response.json();
-  return data.results;
+  let page = 1;
+  let allResults = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(`${API_BASE_URL}/movie/popular?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    
+    allResults = [...allResults, ...data.results];
+    hasMore = page < data.total_pages;
+    page++;
+  }
+
+  return allResults;
 };
 
 export const fetchTopRatedMovies = async () => {
-  const response = await fetch(`${API_BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-BR`);
-  const data = await response.json();
-  return data.results;
+  let page = 1;
+  let allResults = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(`${API_BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    
+    allResults = [...allResults, ...data.results];
+    hasMore = page < data.total_pages;
+    page++;
+  }
+
+  return allResults;
 };
 
 export const fetchNowPlayingMovies = async () => {
-  const response = await fetch(`${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=pt-BR`);
-  const data = await response.json();
-  return data.results;
+  let page = 1;
+  let allResults = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(`${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    
+    allResults = [...allResults, ...data.results];
+    hasMore = page < data.total_pages;
+    page++;
+  }
+
+  return allResults;
 };
 
 export const fetchUpcomingMovies = async () => {
-  const response = await fetch(`${API_BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR`);
-  const data = await response.json();
-  return data.results;
+  let page = 1;
+  let allResults = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(`${API_BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    
+    allResults = [...allResults, ...data.results];
+    hasMore = page < data.total_pages;
+    page++;
+  }
+
+  return allResults;
 };
 
 export const fetchMovieDetails = async (movieId) => {
@@ -43,9 +87,20 @@ export const fetchMovieDetails = async (movieId) => {
 };
 
 export const fetchMoviesByCategory = async (categoryId) => {
-  const response = await fetch(`${API_BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}&language=pt-BR`);
-  const data = await response.json();
-  return data.results;
+  let page = 1;
+  let allResults = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(`${API_BASE_URL}/discover/movie?api_key=${API_KEY}&with_genres=${categoryId}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    
+    allResults = [...allResults, ...data.results];
+    hasMore = page < data.total_pages;
+    page++;
+  }
+
+  return allResults;
 };
 
 export const fetchMovieTrailer = async (movieId) => {
@@ -73,7 +128,18 @@ export const fetchMovieCast = async (movieId) => {
 };
 
 export const fetchMoviesBySearch = async (query) => {
-  const response = await fetch(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=pt-BR`);
-  const data = await response.json();
-  return data.results;
+  let page = 1;
+  let allResults = [];
+  let hasMore = true;
+
+  while (hasMore) {
+    const response = await fetch(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=pt-BR&page=${page}`);
+    const data = await response.json();
+    
+    allResults = [...allResults, ...data.results];
+    hasMore = page < data.total_pages;
+    page++;
+  }
+
+  return allResults;
 };
