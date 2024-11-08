d9c5469a5f474018adb99c04c154586bbe859b5b
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-07 22:21:07 -0300

    Update SearchResults.jsx

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -7,21 +7,45 @@ import MovieCard from '../components/MovieCard';
 const SearchResults = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
   const location = useLocation();
   const query = new URLSearchParams(location.search).get('query');
 
-  useEffect(() => {
-    const getSearchResults = async () => {
-      if (query) {
-        setLoading(true);
-        const results = await fetchMoviesBySearch(query);
-        setSearchResults(results);
-        setLoading(false);
-      }
-    };
+  const fetchAllResults = async () => {
+    setLoading(true);
+    try {
+      const results = await fetchMoviesBySearch(query, page);
+      setSearchResults((prevResults) => [...prevResults, ...results]);
 
-    getSearchResults();
+      if (results.length === 0) {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (query) {
+      setSearchResults([]);
+      setPage(1);
+      setHasMore(true);
+      fetchAllResults();
+    }
   }, [query]);
+
+  useEffect(() => {
+    if (page > 1) {
+      fetchAllResults();
+    }
+  }, [page]);
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
 
   return (
     <div className="px-6 py-8">
@@ -37,6 +61,16 @@ const SearchResults = () => {
               <MovieCard key={movie.id} movie={movie} />
             ))
           )}
+        </div>
+      )}
+      {hasMore && !loading && (
+        <div className="text-center mt-6">
+          <button
+            onClick={handleLoadMore}
+            className="px-6 py-2 bg-[#bd0003] text-white rounded-full"
+          >
+            Carregar mais
+          </button>
         </div>
       )}
     </div>
