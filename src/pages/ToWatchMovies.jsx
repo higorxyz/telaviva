9dfcd3175584619e921f9100a348f99caa324802
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-08 23:00:07 -0300

    Add files via upload

--- src/pages/ToWatchMovies.jsx
+++ src/pages/ToWatchMovies.jsx
@@ -1,14 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { MovieContext } from '../context/MovieContext';
-import { Link } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 
 const ToWatchMovies = () => {
   const { toWatchMovies } = useContext(MovieContext);
   const [loading, setLoading] = useState(true); 
+  const [visibleCount, setVisibleCount] = useState(15);
 
   useEffect(() => {
-    
     const timer = setTimeout(() => {
       setLoading(false);
     }, 500);
@@ -16,7 +16,19 @@ const ToWatchMovies = () => {
     return () => clearTimeout(timer);
   }, []);
 
+  const handleScroll = () => {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
+    setVisibleCount((prevCount) => prevCount + 15);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   if (loading) return <Loading />;
+
+  const displayedMovies = toWatchMovies.slice(0, visibleCount);
 
   return (
     <div className="bg-neutral-950 text-white md:p-6 lg:p-8 xl:p-10 min-h-screen">
@@ -27,13 +39,9 @@ const ToWatchMovies = () => {
         <p className="md:mx-6 lg:mx-8 xl:mx-10">Nenhum filme na lista para ver.</p>
       ) : (
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 md:mx-6 lg:mx-8 xl:mx-10">
-          {toWatchMovies.map((movie) => (
+          {displayedMovies.map((movie) => (
             <div key={movie.id} className="flex-shrink-0">
-              <div className="bg-neutral-800 p-4 rounded-md transition-transform transform hover:scale-105">
-                <h2 className="text-lg font-bold">{movie.title}</h2>
-                <p>Avaliação: {movie.vote_average}</p>
-                <Link to={`/movie/${movie.id}`} className="text-[#bd0003] hover:underline">Ver detalhes</Link>
-              </div>
+              <MovieCard movie={movie} />
             </div>
           ))}
         </div>
