ff931f9c02aed1aaa25656b5efe644d338da9921
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-08 23:38:43 -0300

    Update SearchResults.jsx

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -40,30 +40,41 @@ const SearchResults = () => {
     loadMoreResults();
   }, [page]);
 
-  const handleScroll = (e) => {
-    const bottom = e.target.scrollHeight === e.target.scrollTop + e.target.clientHeight;
-    if (bottom && !loading && hasMore) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 100 >=
+        document.documentElement.scrollHeight
+      ) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <div className="px-6 py-8" onScroll={handleScroll}>
+    <div className="bg-neutral-950 text-white md:p-6 lg:p-8 xl:p-10">
       <h1 className="text-3xl font-bold text-[#bd0003] mb-6">Resultados</h1>
       {loading && page === 1 ? (
         <Loading />
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
           {searchResults.length === 0 ? (
-            <p>Nenhum resultado encontrado.</p>
+            <p className="col-span-full text-center text-red-500">Nenhum resultado encontrado.</p>
           ) : (
             searchResults.map((movie) => (
-              <MovieCard key={movie.id} movie={movie} /> // Utilizando o MovieCard
+              <MovieCard key={movie.id} movie={movie} />
             ))
           )}
         </div>
       )}
-      {loading && page > 1 && <Loading />}
+      {loading && page > 1 && (
+        <div className="flex justify-center py-6">
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
