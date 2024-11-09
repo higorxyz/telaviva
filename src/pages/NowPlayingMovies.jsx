51dd2ed7212822fd1d83c6ae04efbdc9174b5e62
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-08 23:25:20 -0300

    Update NowPlayingMovies.jsx

--- src/pages/NowPlayingMovies.jsx
+++ src/pages/NowPlayingMovies.jsx
@@ -7,36 +7,41 @@ const NowPlayingMovies = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [visibleCount, setVisibleCount] = useState(15);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
 
   useEffect(() => {
     const getMovies = async () => {
       try {
-        const nowPlayingData = await fetchNowPlayingMovies();
-        setMovies(nowPlayingData);
+        setLoadingMore(true);
+        const nowPlayingData = await fetchNowPlayingMovies(page);
+        setMovies((prevMovies) => [...prevMovies, ...nowPlayingData]);
       } catch (err) {
         setError('Erro ao carregar os filmes.');
       } finally {
         setLoading(false);
+        setLoadingMore(false);
       }
     };
     getMovies();
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return;
-      setVisibleCount((prevCount) => prevCount + 15);
+      if (
+        window.innerHeight + document.documentElement.scrollTop + 100 >=
+        document.documentElement.scrollHeight
+      ) {
+        setPage((prevPage) => prevPage + 1);
+      }
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  if (loading) return <Loading />;
+  if (loading && page === 1) return <Loading />;
   if (error) return <div className="text-red-500 text-center mt-6">{error}</div>;
-
-  const displayedMovies = movies.slice(0, visibleCount);
 
   return (
     <div className="bg-neutral-950 text-white md:p-6 lg:p-8 xl:p-10">
@@ -45,12 +50,18 @@ const NowPlayingMovies = () => {
       </h1>
 
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 md:mx-6 lg:mx-8 xl:mx-10">
-        {displayedMovies.map((movie) => (
+        {movies.map((movie) => (
           <div key={movie.id} className="flex-shrink-0">
             <MovieCard movie={movie} />
           </div>
         ))}
       </div>
+      
+      {loadingMore && (
+        <div className="flex justify-center py-6">
+          <Loading />
+        </div>
+      )}
     </div>
   );
 };
