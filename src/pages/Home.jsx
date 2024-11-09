bc330e929788a259d8a26f2e8710ffde174d65aa
Higor Batista <161776354+Hiigorx@users.noreply.github.com>
2024-11-08 23:53:14 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,26 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useContext } from 'react';
 import { fetchPopularMovies, fetchTopRatedMovies, fetchNowPlayingMovies, fetchUpcomingMovies } from '../api';
 import MovieCard from '../components/MovieCard';
 import { Link } from 'react-router-dom';
 import Loading from '../components/Loading';
+import { MovieContext } from '../context/MovieContext';
 
 const Home = () => {
   const [popularMovies, setPopularMovies] = useState([]);
   const [topRatedMovies, setTopRatedMovies] = useState([]);
   const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
   const [upcomingMovies, setUpcomingMovies] = useState([]);
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const { toWatchMovies, watchedMovies } = useContext(MovieContext);
 
   const popularRef = useRef(null);
   const topRatedRef = useRef(null);
   const nowPlayingRef = useRef(null);
   const upcomingRef = useRef(null);
+  const recommendedRef = useRef(null);
 
   useEffect(() => {
     const getMovies = async () => {
@@ -38,6 +43,11 @@ const Home = () => {
     };
     getMovies();
   }, []);
+
+  useEffect(() => {
+    const allMovies = [...toWatchMovies, ...watchedMovies];
+    setRecommendedMovies(allMovies);
+  }, [toWatchMovies, watchedMovies]);
 
   const scrollLeft = (ref) => {
     if (ref.current) {
@@ -64,6 +74,7 @@ const Home = () => {
   const displayedTopRatedMovies = topRatedMovies.slice(0, 12);
   const displayedNowPlayingMovies = nowPlayingMovies.slice(0, 12);
   const displayedUpcomingMovies = upcomingMovies.slice(0, 12);
+  const displayedRecommendedMovies = recommendedMovies.slice(0, 12);
 
   return (
     <div className="bg-neutral-950 text-white md:p-6 lg:p-8 xl:p-10">
@@ -88,8 +99,26 @@ const Home = () => {
           →
         </button>
       </div>
+      <h1 className="text-4xl font-bold my-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
+        Filmes Recomendados
+      </h1>
+      <div className="flex items-center relative">
+        <button onClick={() => scrollLeft(recommendedRef)} className="absolute left-0 -translate-x-1/2 transform p-4 bg-[#bd0003] rounded-full hover:bg-red-500">
+          ←
+        </button>
+        <div ref={recommendedRef} className="flex overflow-x-auto space-x-4 pb-4 mx-0 md:mx-6 lg:mx-8 xl:mx-10">
+          {displayedRecommendedMovies.map((movie) => (
+            <div key={movie.id} className="flex-shrink-0 w-48 sm:w-48 md:w-48 lg:w-56 xl:w-64">
+              <MovieCard movie={movie} />
+            </div>
+          ))}
+        </div>
+        <button onClick={() => scrollRight(recommendedRef)} className="absolute right-0 translate-x-1/2 transform p-4 bg-[#bd0003] rounded-full hover:bg-red-500">
+          →
+        </button>
+      </div>
 
-      <h1 className="text-4xl font-bold mt-12 mb-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
+      <h1 className="text-4xl font-bold my-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
         Melhores Avaliados
         <Link to="/top-rated-movies" className="bg-[#bd0003] text-white py-1 px-3 rounded-full text-sm">
           Ver Todos
@@ -111,7 +140,7 @@ const Home = () => {
         </button>
       </div>
 
-      <h1 className="text-4xl font-bold mt-12 mb-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
+      <h1 className="text-4xl font-bold my-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
         Filmes Populares
         <Link to="/popular-movies" className="bg-[#bd0003] text-white py-1 px-3 rounded-full text-sm">
           Ver Todos
@@ -133,9 +162,9 @@ const Home = () => {
         </button>
       </div>
 
-      <h1 className="text-4xl font-bold mt-12 mb-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
-        Próximos Lançamentos
-        <Link to="/upcoming-movies" className="bg-[#bd0003] text-white py-1 px-3 rounded-full text-sm">
+      <h1 className="text-4xl font-bold my-8 flex justify-between items-center md:mx-6 lg:mx-8 xl:mx-10">
+        Filmes para Ver Depois
+        <Link to="/to-watch-movies" className="bg-[#bd0003] text-white py-1 px-3 rounded-full text-sm">
           Ver Todos
         </Link>
       </h1>
